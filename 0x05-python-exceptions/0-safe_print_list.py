64ca9b88176ccc58f9b1e75961b5867b9559668a
Ngwanatuka <ngwanatuka@gmail.com>
2022-12-20 01:47:03 +0200

    fixing code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,16 +3,17 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end="")
             counter += 1
         print()
         return counter
     except IndexError:
         for i in range(x):
             try:
-                print(my_list[i], end=' ')
+                print(my_list[i], end="")
             except IndexError:
                 break
+            else:
             counter += 1
         print()
         return counter
