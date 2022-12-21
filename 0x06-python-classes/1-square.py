7ad5b79bb1ce8d8dce386ca1508407f4d9868d99
Ngwanatuka <ngwanatuka@gmail.com>
2022-12-21 14:09:02 +0200

    new square file added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains the Square class, which represents a square.
+"""
+
+class Square:
+    """
+    Class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
