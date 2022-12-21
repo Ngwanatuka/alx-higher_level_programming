1f60a73b44b647082c4af1f0f8b79597adda55c9
Ngwanatuka <ngwanatuka@gmail.com>
2022-12-21 12:13:50 +0200

    first commit for this project

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """ A class representing a square.
+
+    This class does not have any attributes or methods, and is 
+    simply a placeholder for future development.
+    """
+
+    pass
