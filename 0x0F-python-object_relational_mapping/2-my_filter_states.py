f2614811b9277d603c3e20a53f7668435517a354
Ngwanatuka <Ngwanatuka@gmail.com>
2023-03-24 08:37:20 +0200

    final touches of task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,56 +1,25 @@
 #!/usr/bin/python3
-"""
-This module runs a script that searches the states table
-for a given state name.
-"""
+""" script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument."""
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    """
-    get the command line arguments
-    """
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == "__main__":
 
-    """
-    connect to the database
-    """
-    db = MySQLdb.connect(
-        host='localhost',
-        user=mysql_user,
-        passwd=mysql_password,
-        db=db_name,
-        port=3306
-    )
-
-    """
-    get a cursor object
-    """
-    cursor = db.cursor()
-
-    """
-    execute the query
-    """
-    query = "SELECT * FROM states WHERE\
-            name = '{}' ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    """
-    get result
-    """
-    result = cursor.fetchall()
-
-    """
-    print the results
-    """
-    for row in result:
-        print(row)
-
-    """
-    close the database connection
-    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY states.id ASC;"
+        .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
     db.close()
