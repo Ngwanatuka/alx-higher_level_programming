a8aa1cfad5fc713e5dc1497586f817b180dbebee
Ngwanatuka <Ngwanatuka@gmail.com>
2023-04-02 21:09:18 +0200

    fixing code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-Sens a POST request with a letter as a parameter and
-displays the response in a particular format.
+    Sends a POST request with a letter as a parameter and
+    displays the response in a particular format.
 """
-
 import sys
 import requests
 
 
-if __name__ == '__mian__':
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     else:
         letter = ""
-    url = "http://26550f78c48a.50da2fcc.alx-cod.online:5000/search_user"
-    response = request.post(url, data={"q": letter})
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={"q": letter})
 
     try:
-        reponse_dict = response.json()
+        response_dict = response.json()
         if response_dict:
             print("[{}] {}".format(
                 response_dict.get('id'), response_dict.get('name')))
