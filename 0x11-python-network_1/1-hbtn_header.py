c46ea94722974bba2f95b63e15ded29ae6fac979
Ngwanatuka <Ngwanatuka@gmail.com>
2023-04-02 17:23:52 +0200

    script to retrieve X-Request-Id header from a URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-""" a script that takes in a URL, sends a request
-to the URL and display the value of the X-Requested-Id
-"""
 
-import urllib.request
+"""Module to get the value of X-Request-Id header"""
+
 import sys
+import urllib.request
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+
+def get_request_id(url: str) -> str:
+    """
+    Get the value of X-Request-Id header
+    Args: url (str): URL to send the request
+    Request: str: Value of the X-Rquest_Id header in the response
+    """
+
     req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        headers = res.info()
+        x_request_id = headers.get('X-Request-Id')
+        return x_request_id
 
-with urllib.request.urlopen(req) as response:
-    print(response.headers.get("X-Request-Id"))
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    x_request_id = get_request_id(url)
+    print(x_request_id)
